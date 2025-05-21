a196bc0a64577bc221a9799db12a38ff117730f7
Kylian <kylian.patry@gmail.com>
2025-05-21 14:51:22 +0200

    Add refresh button

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,9 @@ import { faker } from "@faker-js/faker";
 
 const searchQuery = ref("");
 const products = ref<Product[]>([]);
-const loading = ref(false);
+const loading = ref(true);
 
 onMounted(async () => {
-  loading.value = true;
   products.value = getProducts();
   loading.value = false;
 });
@@ -17,6 +16,13 @@ onMounted(async () => {
 const handleAddProductToCart = (product: Product) => {
   const addedProduct = { ...product, cartId: faker.string.uuid() };
   addProductToCart(addedProduct);
+};
+
+const refreshProducts = async () => {
+  loading.value = true;
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  products.value = getProducts();
+  loading.value = false;
 };
 </script>
 <template>
@@ -45,10 +51,18 @@ const handleAddProductToCart = (product: Product) => {
     </div>
     <div class="flex justify-center items-center">
       <div
-        class="flex flex-col items-center border-2 border-gray-300 rounded-md p-2 w-3/4 h-1/2 mt-5"
+        class="flex flex-col items-center border-2 border-gray-300 rounded-md p-2 w-3/4 h-1/2 mt-5 relative"
       >
+        <button
+          @click="refreshProducts"
+          class="absolute top-1 right-2 bg-green-500 text-white p-2 rounded-md hover:bg-green-600 cursor-pointer"
+          :disabled="loading"
+        >
+          <span v-if="!loading">Refresh</span>
+          <span v-else>Chargement...</span>
+        </button>
         <div
-          v-if="products.length > 0"
+          v-if="products.length > 0 && !loading"
           class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4"
         >
           <ProductCard
@@ -58,7 +72,7 @@ const handleAddProductToCart = (product: Product) => {
             @addProductToCart="handleAddProductToCart"
           />
         </div>
-        <div v-else-if="loading">
+        <div v-else-if="loading" class="flex justify-center items-center p-8">
           <div
             class="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"
           ></div>
