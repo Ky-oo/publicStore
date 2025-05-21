60238d933340dac2b9347ef3678d23fa18de7877
Kylian <kylian.patry@gmail.com>
2025-05-21 15:29:17 +0200

    Add CORS policy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,16 @@ export default defineNuxtConfig({
   runtimeConfig: {
     password: process.env.NUXT_PASSWORD,
   },
+  nitro: {
+    routeRules: {
+      "/api/**": {
+        cors: true,
+        headers: {
+          "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*",
+        },
+      },
+    },
+  },
 });
