773d787382966ee5b4ceb0513e8e0c93f59c60a6
Kylian <kylian.patry@gmail.com>
2025-05-21 12:26:36 +0200

    Create index Front (div product + searchbar)

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const searchQuery = ref("");
+const products = ref([]);
+</script>
 <template>
   <div>
-    <h1>Store</h1>
+    <div class="flex justify-center flex-col items-center">
+      <h1 class="text-3xl font-bold underline">Store</h1>
+      <input
+        class="border-2 border-gray-300 rounded-md p-2 w-1/2 mt-5"
+        type="text"
+        v-model="searchQuery"
+      />
+      <button class="bg-blue-500 text-white p-2 rounded-md mt-5">Search</button>
+    </div>
+    <div class="flex justify-center">
+      <div
+        v-if="products.length > 0"
+        class="flex justify-center flex-col items-center border-2 border-gray-300 rounded-md p-2 w-1/2 mt-5"
+      >
+        <div v-for="product in products" :key="product.id">
+          <span>Product</span>
+        </div>
+      </div>
+      <div
+        v-else
+        class="flex justify-center flex-col items-center border-2 border-gray-300 rounded-md p-2 w-1/2 h-1/2 mt-5"
+      >
+        <span>No products found</span>
+      </div>
+    </div>
   </div>
 </template>
