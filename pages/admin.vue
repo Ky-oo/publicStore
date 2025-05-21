8968b1150b940e3dcb7256f08c23d08656da00ac
Kylian <kylian.patry@gmail.com>
2025-05-21 13:11:58 +0200

    Save created products in store

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import { addProducts } from "../stores/productsStore";
+import type { Product } from "../types/products";
+
 const products = ref([]);
+const loading = ref(false);
+
 const createProducts = async () => {
-  const response = await $fetch("/api/products", {
+  loading.value = true;
+  const response = await $fetch<{ products: Product[] }>("/api/products", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -10,7 +16,9 @@ const createProducts = async () => {
       quantity: 200,
     },
   });
-  console.log(response);
+
+  addProducts(response.products as Product[]);
+  loading.value = false;
 };
 </script>
 <template>
@@ -19,7 +27,11 @@ const createProducts = async () => {
       <h1 class="text-3xl font-bold underline">Admin</h1>
 
       <button
-        :class="products.length == 200 ? 'disabled bg-gray-500' : 'bg-blue-500'"
+        :class="
+          products.length == 200 || loading
+            ? 'disabled bg-gray-500'
+            : 'bg-blue-500'
+        "
         @click="createProducts"
         class="bg-blue-500 text-white p-2 rounded-md mt-5 cursor-pointer hover:bg-blue-600"
       >
