fa490ca4d3ab5c694a980c636b2b9d70d7967ef4
Kylian <kylian.patry@gmail.com>
2025-05-21 12:36:54 +0200

    Create Admin Front

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,6 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const products = ref([]);
+</script>
 <template>
   <div>
-    <h1>Admin</h1>
+    <div class="flex justify-center flex-col items-center">
+      <h1 class="text-3xl font-bold underline">Admin</h1>
+
+      <button
+        :class="products.length == 0 ? 'disabled bg-gray-500' : 'bg-blue-500'"
+        class="bg-blue-500 text-white p-2 rounded-md mt-5"
+      >
+        Creat 200 Product
+      </button>
+      <div v-if="products.length == 0">
+        <span class="text-red-500">Product already created</span>
+      </div>
+    </div>
   </div>
 </template>
